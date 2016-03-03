e7337db842c5070f972b5c921f753b22dade26e6
hawtim zhang <vastfield@163.com>
2016-03-03 14:33:23 +0800

    finish the static pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+window.onload = function (){
+	$(window).scroll(function(){
+		var n =$(window).scrollTop()/ $(window).height();
+		$("#l-nav ul li").eq
+	})
+}
---
+++ js/response.js
@@ -0,0 +1,14 @@
+// js判断页面大小的变化实现根元素字体大小的变换
+(function(doc, win) {
+  var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function() {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      docEl.style.fontSize = 10 * (clientWidth / 320) + 'px';
+    };
+
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
